a184fc38479a36aac753a69225b088386d48b8c2
ks-ahmed <ka-sharif@outlook.com>
2025-09-23 13:17:40 +0100

    update

--- @app/lib/src/GraphileApolloLink.client.ts
+++
@@ -1,7 +0,0 @@
-import { ApolloLink } from "@apollo/client";
-export class GraphileApolloLink extends ApolloLink {
-  constructor() {
-    super();
-    throw new Error(`This should not be called on the client`);
-  }
-}
